1946a47510ef93e62852ede00f6d22f973f7356c
Alexander Booth <adbubc+1@gmail.com>
2016-11-22 23:10:23 -0800

    initial commit

---
+++ fract.hs
@@ -0,0 +1,39 @@
+import Codec.Picture
+import Codec.Picture.Types
+import System.Environment
+import System.Directory
+import System.Random
+
+import Codec.Picture
+import Control.Monad
+import Control.Monad.ST
+
+main :: IO ()
+main = do
+    putStrLn "Beginning rendering of image."
+    savePngImage "output.png" generateFractal
+    putStrLn "Completed"
+
+
+width = 1920
+height = 1080
+
+generateFractal :: DynamicImage
+generateFractal = ImageRGB8 $ generateImage mandelbrot width height
+
+iters = 25
+
+palette = foldr (\a -> \b -> (PixelRGB8 (fromIntegral a*10) (fromIntegral a*10) 0):b) [] [0..iters]
+
+mandelbrot :: Int -> Int -> PixelRGB8
+mandelbrot x0 y0 = getColor 0 0 0
+    where getColor :: Float -> Float -> Int -> PixelRGB8
+          getColor x y i = if x*x + y*y < 2*2 && i < iters 
+                           then getColor (x*x - y*y + (scaleX x0)) (2*x*y + (scaleY y0)) (i+1)
+                           else palette!!i
+
+scaleX :: Int -> Float
+scaleX x = (3.5 * (fromIntegral x)) / (fromIntegral width) - 2.5
+
+scaleY :: Int -> Float
+scaleY y = (2.0 * (fromIntegral y)) / (fromIntegral height) - 1.0
